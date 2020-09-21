6e2654f6cf52cf397a10c4841f8f24b4d054cfdb
bwapes <benwaples@gmail.com>
2020-09-21 16:32:06 -0700

    createResponse.fn, / and /red routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,20 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
-  it('dummy test', () => {
-    
+
+  it('should return html saying hi', async() => {
+    const response = await request(app)
+      .get('/')
+
+
+    expect(response.text).toEqual('hi')
+
+  })
+
+  it('should return HTML document with the title red', async() => {
+    const response = await request(app)
+      .get('/red')
+
+    expect(response.text).toEqual('<html><body><h1>Red</h1></body></html>')
   })
 });
